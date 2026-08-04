abc21940c57ee5a8c5003b04c2cbf3f89f46ed47
Juan Montoya <montoyajuanes11@gmail.com>
2026-08-03 20:57:04 -0500

    chore(models): create associations structure

---
+++ models/asociaciones.js
@@ -0,0 +1,27 @@
+import Usuario from "./Usuario.js";
+import Rol from "./Rol.js";
+
+import Paciente from "./Paciente.js";
+import Medico from "./Medico.js";
+
+import Especialidad from "./Especialidad.js";
+import Horario from "./Horario.js";
+
+import EstadoCita from "./EstadoCita.js";
+import Cita from "./Cita.js";
+
+import Diagnostico from "./Diagnostico.js";
+import Prescripcion from "./Prescripcion.js";
+
+import Notificacion from "./Notificacion.js";
+import PlantillaNotificacion from "./PlantillaNotificacion.js";
+
+import ConfiguracionNotificacion from "./ConfiguracionNotificacion.js";
+import ColaNotificacion from "./ColaNotificacion.js";
+
+import SolicitudWhatsApp from "./SolicitudWhatsApp.js";
+import Token from "./Token.js";
+
+export default function asociarModelos() {
+
+}
